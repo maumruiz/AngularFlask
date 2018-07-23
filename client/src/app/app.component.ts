78f0f14ed92509de1a530ecb3b9af8257f605d44
maumruiz <mau.mruiz@hotmail.com>
2018-07-22 19:31:13 -0500

    Angular files

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  title = 'My App';
+  serverData: JSON;
+  employeeData: JSON;
+  greeting: string = '';
+  employees: any[] = [];
+
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit() {
+    this.sayHi();
+    this.getAllEmployees();
+  }
+
+  sayHi() {
+    this.httpClient.get('http://127.0.0.1:5002/').subscribe(data => {
+      this.serverData = data as JSON;
+      console.log(this.serverData);
+      this.greeting = this.serverData['text'];
+    })
+  }
+
+  getAllEmployees() {
+    this.httpClient.get('http://127.0.0.1:5002/employees').subscribe(data => {
+      this.employeeData = data as JSON;
+      console.log(this.employeeData);
+      this.employees = this.employeeData['employees'];
+    })
+  }
 }
